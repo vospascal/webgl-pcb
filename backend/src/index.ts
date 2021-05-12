ba5fda6b5b3f911cfa643dc2bd4e0bdd53d1cef5
Pascal Vos <vospascal@gmail.com>
2021-05-12 20:47:33 +0200

    fix small things

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,6 @@ moduleAlias.addAliases({
 import path from 'path';
 import express from 'express';
 import * as http from 'http';
-import cors from 'cors';
 import { json, urlencoded } from 'body-parser';
 import compress from 'compression';
 import helmet from 'helmet';
@@ -19,6 +18,7 @@ import morganMiddleware from '~/middlewares/morganMiddleware';
 import { BaseRoutes } from '~/routes/BaseRoutes';
 import { AppRoutes } from '~/routes/appRoutes';
 import { DefaultRoutes } from '~/routes/defaultRoutes';
+import cors from '~/middlewares/cors';
 
 //getting environment config
 const envFileName = `.env.${process.env.NODE_ENV}`;
@@ -35,8 +35,7 @@ app.use(compress()); // gzip compression
 app.use(helmet()); // making it all secure by setting various HTTP headers
 app.use(json()); // json parsing requests and attache them to req.body
 app.use(urlencoded({ extended: true })); // parses urlencoded bodies like forms and attache them to req.body
-app.use(cors()); // allowing cors origin
-app.options('*', cors());
+app.use(cors); // allowing cors origin
 app.use(morganMiddleware); // added logging
 
 // here we are adding the AppRoutes to our array,
@@ -44,14 +43,19 @@ const routes: Array<BaseRoutes> = [];
 routes.push(new AppRoutes(app));
 routes.push(new DefaultRoutes(app));
 
-try {
-    // start server on configurated port
-    server.listen(port, () => {
+// start server on configurated port
+server
+    .listen(port, () => {
         Logger.info(`⚡⚡ Server running at http://localhost:${port} ⚡⚡`);
         routes.forEach((route: BaseRoutes) => {
             Logger.info(`Routes configured for ${route.getName()}`);
         });
+    })
+    .once('error', (error: any) => {
+        if (error?.code === 'EADDRINUSE') {
+            Logger.error(`Port ${port} is already in use`);
+            process.exit(1);
+        } else {
+            Logger.error(`Error: ${error}`);
+        }
     });
-} catch (error) {
-    Logger.error(`Error: ${error}`);
-}
